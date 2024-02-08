277c0702b165d74ccb01c0c722ad244e8c33630e
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-02-08 22:36:45 +0600

    List Remove

---
+++ ListRemove.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  var number =[1,2,3,4,5,6,7,8,9,10];
+
+  number.removeLast();//it remove last item
+  number.remove(5); // it remove by input value
+  number.removeAt(3); //it remove by input index
+
+  print(number);
+}
