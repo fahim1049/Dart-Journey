0325022079d1e3d07978e75accb1af6046c33401
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-02-08 21:28:57 +0600

    const and fInal keyword

---
+++ Unary.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  var x = 1;
+  print("Prefix Unary:\n");
+
+//prefix
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print("Postfix Unary:\n");
+ //postfix
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+
+
+}
