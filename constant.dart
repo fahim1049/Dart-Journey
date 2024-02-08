98935f0f3af1a036514c685803424251e842fe85
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-02-08 21:31:23 +0600

    Const and final keyword

---
+++ constant.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  const age = 23;
+
+  final id = 1049;
+
+  print(age);
+  print(id);
+
+
+
+  /*When we use this keyword then value can't change
+
+                  @author fahim
+
+                  */
+}
