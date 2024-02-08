ef4cc9156f0c477a5be10189b1b7064657cf7375
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-02-08 21:57:08 +0600

    List Properties

---
+++ ListPropertise.dart
@@ -0,0 +1,24 @@
+void main()
+{
+    var city = ['Bhola','Charfassion','Lamohon','Jhanpur'];
+    var city2 = ['Atkopat'];
+
+   var result = city.length;// it shows list length
+   var a = city.first;// it shows first item
+   var b = city.isEmpty;// it shows list is empty or not return boolean value
+
+   var c = city.isNotEmpty; //it shows list empty or not return boolean value
+   var d = city.reversed; //it shows list last item first and first item last mens reverse
+   var e = city2.single;//when item one it return,when item more it can't return
+
+    print(result);
+    print(a);
+    print(b);
+    print(c);
+    print(d);
+    print(e);
+
+
+
+
+}
